4b041e61bf17ac5c42b612a505ec2f6bf060e9ae
yatihe01 <yatihe769@gmail.com>
2025-10-15 15:13:26 +0800

    Modify AddProgressCommand to pass checkstyle

--- src/main/java/tutortrack/logic/commands/AddProgressCommand.java
+++ src/main/java/tutortrack/logic/commands/AddProgressCommand.java
@@ -1,38 +1,14 @@
 package tutortrack.logic.commands;
 
 import static java.util.Objects.requireNonNull;
-import static tutortrack.logic.parser.CliSyntax.PREFIX_ADDRESS;
-import static tutortrack.logic.parser.CliSyntax.PREFIX_COST;
-import static tutortrack.logic.parser.CliSyntax.PREFIX_DAYTIME;
-import static tutortrack.logic.parser.CliSyntax.PREFIX_EMAIL;
 import static tutortrack.logic.parser.CliSyntax.PREFIX_LESSON_PROGRESS;
-import static tutortrack.logic.parser.CliSyntax.PREFIX_NAME;
-import static tutortrack.logic.parser.CliSyntax.PREFIX_PHONE;
-import static tutortrack.logic.parser.CliSyntax.PREFIX_SUBJECTLEVEL;
-import static tutortrack.logic.parser.CliSyntax.PREFIX_TAG;
-import static tutortrack.model.Model.PREDICATE_SHOW_ALL_PERSONS;
-
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.HashSet;
 import java.util.List;
-import java.util.Objects;
-import java.util.Optional;
-import java.util.Set;
-
 import tutortrack.commons.core.index.Index;
-import tutortrack.commons.util.CollectionUtil;
-import tutortrack.commons.util.ToStringBuilder;
 import tutortrack.logic.Messages;
 import tutortrack.logic.commands.exceptions.CommandException;
 import tutortrack.model.Model;
-import tutortrack.model.person.Address;
-import tutortrack.model.person.Email;
 import tutortrack.model.lesson.LessonProgress;
-import tutortrack.model.person.Name;
 import tutortrack.model.person.Person;
-import tutortrack.model.person.Phone;
-import tutortrack.model.tag.Tag;
 
 /**
  * Edits the details of an existing lesson in the address book.

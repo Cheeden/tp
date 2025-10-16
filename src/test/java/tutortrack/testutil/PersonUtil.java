5f29abca41f3166e8e20920f1b943d018e42402c
Li Mingyu <e1484123@u.nus.edu>
2025-10-16 17:13:57 +0800

    Fix more style error

--- src/test/java/tutortrack/testutil/PersonUtil.java
+++ src/test/java/tutortrack/testutil/PersonUtil.java
@@ -1,14 +1,5 @@
 package tutortrack.testutil;
 
-import java.util.List;
-import java.util.Set;
-
-import tutortrack.logic.commands.AddCommand;
-import tutortrack.logic.commands.EditCommand.EditPersonDescriptor;
-import tutortrack.model.lesson.LessonProgress;
-import tutortrack.model.person.Person;
-import tutortrack.model.tag.Tag;
-
 import static tutortrack.logic.parser.CliSyntax.PREFIX_ADDRESS;
 import static tutortrack.logic.parser.CliSyntax.PREFIX_COST;
 import static tutortrack.logic.parser.CliSyntax.PREFIX_DAYTIME;
@@ -17,6 +8,15 @@ import static tutortrack.logic.parser.CliSyntax.PREFIX_NAME;
 import static tutortrack.logic.parser.CliSyntax.PREFIX_PHONE;
 import static tutortrack.logic.parser.CliSyntax.PREFIX_SUBJECTLEVEL;
 import static tutortrack.logic.parser.CliSyntax.PREFIX_TAG;
+
+import java.util.List;
+import java.util.Set;
+
+import tutortrack.logic.commands.AddCommand;
+import tutortrack.logic.commands.EditCommand.EditPersonDescriptor;
+import tutortrack.model.lesson.LessonProgress;
+import tutortrack.model.person.Person;
+import tutortrack.model.tag.Tag;
 
 /**
  * A utility class for Person.

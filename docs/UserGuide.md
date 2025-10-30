12e91901c81fb2ea08a5eddb19743d8b266f9b85
yatihe01 <yatihe769@gmail.com>
2025-10-30 13:58:37 +0800

    Modify intro

--- docs/UserGuide.md
+++ docs/UserGuide.md
@@ -3,7 +3,7 @@ layout: page
 title: User Guide
 ---
 
-TutorTrack is a **desktop app for managing contacts, optimized for use via a Command Line Interface** (CLI) while still having the benefits of a Graphical User Interface (GUI). If you can type fast, TutorTrack can get your contact management tasks done faster than traditional GUI apps.
+TutorTrack is a **desktop app for managing student contacts, optimized for use via a Command Line Interface** (CLI) while still having the benefits of a Graphical User Interface (GUI). If you can type fast, TutorTrack can help you track lesson plans, assignments, deadlines, and student progress more efficiently faster than traditional GUI apps.
 
 * Table of Contents
 {:toc}
--- src/main/java/tutortrack/logic/parser/ParserUtil.java
+++ src/main/java/tutortrack/logic/parser/ParserUtil.java
@@ -27,12 +27,13 @@ import tutortrack.model.tag.Tag;
  */
 public class ParserUtil {
 
-    public static final String MESSAGE_INVALID_INDEX = "Index is not a non-zero unsigned integer.";
+    public static final String MESSAGE_INVALID_INDEX =
+            "Invalid index. Index should be a positive integer.";
 
     /**
      * Parses {@code oneBasedIndex} into an {@code Index} and returns it. Leading and trailing whitespaces will be
      * trimmed.
-     * @throws ParseException if the specified index is invalid (not non-zero unsigned integer).
+     * @throws ParseException if the specified index is invalid (not positive integer).
      */
     public static Index parseIndex(String oneBasedIndex) throws ParseException {
         String trimmedIndex = oneBasedIndex.trim();

a82e76ead5b4152147c9744f211eea97c5f2bed6
Li Mingyu <e1484123@u.nus.edu>
2025-10-30 16:44:23 +0800

    Update tests for edit commands

---
+++ src/test/java/tutortrack/logic/commands/EditPlanCommandTest.java
@@ -0,0 +1,68 @@
+package tutortrack.logic.commands;
+
+import static tutortrack.logic.commands.CommandTestUtil.assertCommandFailure;
+import static tutortrack.testutil.Assert.assertThrows;
+import static tutortrack.testutil.TypicalIndexes.INDEX_FIRST_PERSON;
+import static tutortrack.testutil.TypicalPersons.getTypicalAddressBook;
+
+import java.time.LocalDate;
+
+import org.junit.jupiter.api.Test;
+
+import tutortrack.commons.core.index.Index;
+import tutortrack.logic.Messages;
+import tutortrack.logic.commands.exceptions.CommandException;
+import tutortrack.logic.parser.ParserUtil;
+import tutortrack.logic.parser.exceptions.ParseException;
+import tutortrack.model.Model;
+import tutortrack.model.ModelManager;
+import tutortrack.model.UserPrefs;
+import tutortrack.model.lesson.LessonPlan;
+import tutortrack.model.person.Person;
+
+public class EditPlanCommandTest {
+
+    private Model model = new ModelManager(getTypicalAddressBook(), new UserPrefs());
+
+    @Test
+    void execute_invalidIndexOutOfBounds_throwsCommandException() throws ParseException {
+        Index targetIndex = Index.fromOneBased(model.getFilteredPersonList().size() + 1);
+        LessonPlan plan = ParserUtil.parseLessonPlan("2025-10-21|Updated Plan");
+        EditPlanCommand command = new EditPlanCommand(targetIndex, plan);
+
+        assertCommandFailure(command, model, Messages.MESSAGE_INVALID_PERSON_DISPLAYED_INDEX);
+    }
+
+    @Test
+    void execute_noExistingPlan_throwsCommandException() throws ParseException {
+        Person person = model.getFilteredPersonList().get(INDEX_FIRST_PERSON.getZeroBased());
+        LessonPlan toEdit = ParserUtil.parseLessonPlan("2025-10-31|Updated algebra concepts");
+        EditPlanCommand command = new EditPlanCommand(Index.fromOneBased(1), toEdit);
+
+        String expectedMessage = String.format(EditPlanCommand.MESSAGE_NOT_FOUND, toEdit.getDate());
+        assertThrows(CommandException.class, expectedMessage, () -> command.execute(model));
+    }
+
+    @Test
+    void execute_successfullyEditsExistingPlan() throws ParseException, CommandException {
+        Person person = model.getFilteredPersonList().get(INDEX_FIRST_PERSON.getZeroBased());
+        LocalDate targetDate = LocalDate.of(2025, 10, 25);
+        LessonPlan original = new LessonPlan(targetDate, "Original Plan");
+        person.addLessonPlan(original);
+
+        LessonPlan updated = new LessonPlan(targetDate, "Updated Plan");
+        EditPlanCommand command = new EditPlanCommand(Index.fromOneBased(1), updated);
+
+        CommandResult result = command.execute(model);
+
+        String expectedMessage = String.format(EditPlanCommand.MESSAGE_SUCCESS_EDIT,
+                updated.getDate(), updated.getPlan());
+        assert result.getFeedbackToUser().equals(expectedMessage);
+    }
+
+    @Test
+    void parseLessonPlan_invalidFormat_throwsParseException() {
+        assertThrows(ParseException.class, () ->
+                ParserUtil.parseLessonPlan("invalid date|description"));
+    }
+}
